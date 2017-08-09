d3f490455e264d8889ab580e765b45e643108241
DarushDev <darushfaqiri@gmail.com>
2017-08-09 20:26:02 +0800

    Add app classes

---
+++ app/src/main/java/com/example/myandroiddagger2/app/Constants.java
@@ -0,0 +1,14 @@
+package com.example.myandroiddagger2.app;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.nal.usda.gov";
+
+    public static final String API_KEY = "tigVqk5iiQ9FrIRVK8GexdgMzdu9Yc8yCtRAbLNE";
+
+    public static final String SUGAR_NUTRIENT = "269";
+
+    public static final int YELLOW_LEVEL = 10;
+
+    public static final int RED_LEVEL = 20;
+}
---
+++ app/src/main/java/com/example/myandroiddagger2/app/DeezFoodzApplication.java
@@ -0,0 +1,11 @@
+package com.example.myandroiddagger2.app;
+
+import android.app.Application;
+
+public class DeezFoodzApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
