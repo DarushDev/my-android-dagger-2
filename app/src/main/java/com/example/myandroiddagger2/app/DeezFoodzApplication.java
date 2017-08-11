29590df09e3fb3e6942ae780eaa08d2f6c5b8fb0
DarushDev <darushfaqiri@gmail.com>
2017-08-11 15:05:46 +0800

    Initializes the appComponent field when the application first starts up.

--- app/src/main/java/com/example/myandroiddagger2/app/DeezFoodzApplication.java
+++ app/src/main/java/com/example/myandroiddagger2/app/DeezFoodzApplication.java
@@ -2,10 +2,28 @@ package com.example.myandroiddagger2.app;
 
 import android.app.Application;
 
+import com.example.myandroiddagger2.dagger.AppComponent;
+import com.example.myandroiddagger2.dagger.AppModule;
+import com.example.myandroiddagger2.dagger.DaggerAppComponent;
+
 public class DeezFoodzApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        appComponent = initDagger(this);
     }
+
+    private AppComponent appComponent;
+
+    public AppComponent getAppComponent() {
+        return appComponent;
+    }
+
+    protected AppComponent initDagger(DeezFoodzApplication application) {
+        return DaggerAppComponent.builder()
+                .appModule(new AppModule(application))
+                .build();
+    }
+
 }
---
+++ app/src/main/java/com/example/myandroiddagger2/dagger/AppComponent.java
@@ -0,0 +1,11 @@
+package com.example.myandroiddagger2.dagger;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface AppComponent {
+
+}
